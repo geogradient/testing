9b2a50d5cbf8069482ab47cf7b1c44ead0e06251
Jose Beltran <beltran.data@gmail.com>
2014-06-16 17:49:34 +0200

    Working versio with TODO remove filterList conditional

---
+++ gpt_config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  2 13:41:38 2014
+@author: Jose Beltran
+VISAT-BEAM configuration module
+"""
+###############################
+# Change these accordingly
+#beamHome          = '/home/jobel/beam-4.11/'
+#beamBinDir         = beamHome + 'bin/'
+#gptProcessor       = beamBinDir + 'gpt.sh'
+#pconvertProcessor = beamBinDir + 'pconvert.sh'
+###############################
+
+###############################
+# Change these accordingly
+beamHome           = '/home/jobel/beam-5.0/'
+beamBinDir         = beamHome + 'bin/'
+gptProcessor       = beamBinDir + 'gpt.sh'
+#pconvertProcessor = beamBinDir + 'pconvert.sh'
+###############################
